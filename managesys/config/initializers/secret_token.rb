730f4e9f82dcca9052688ced06d33b36db3e6647
root <root@wolver.(none)>
2013-08-09 12:25:41 +0800

    first commit

---
+++ managesys/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Managesys::Application.config.secret_token = '5678c5d4993ed489b74a98076aac677be13aaeeaf9024d08ec0e1999e2fa49ac14b4178c6bbed9e4383c3bdf851fd6ba7b54bac0b9a2b17db882d99c60d2f0e3'
